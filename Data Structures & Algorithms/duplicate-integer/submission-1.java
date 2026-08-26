51a77edab8e5af5215a8dff37a8e5f930a68b014
kal <65039699+kal-koder@users.noreply.github.com>
2026-08-26 21:06:10 +0200

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/duplicate-integer/submission-1.java
@@ -0,0 +1,13 @@
+// import java.util.HashSet;
+// import java.util.Set;
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+        Set<Integer> seen = new HashSet<Integer>();
+        for (int i = 0; i < nums.length; i++) {
+            if (seen.contains(nums[i])) {
+                return true;
+            } else {seen.add(nums[i]);}
+        }
+        return false;
+    }
+}
